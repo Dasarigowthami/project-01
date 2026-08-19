45181126851335e4fde99777e1ac846ce1858d77
Dasarigowthami <dasarigowthamiroyal539@gmail.com>
2026-08-19 12:42:28 +0530

    demo file has been created

---
+++ demo1.js
@@ -0,0 +1,16 @@
+const { log } = require("node:console");
+
+function add(a,b){
+    return a+b;
+}
+// console.log();
+
+function sub(a,b){
+    return a-b;
+}
+
+
+
+
+// console.log(add(3,5)); 
+module.exports={add,sub};
